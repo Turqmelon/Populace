e344830ca4ef8a93af6b254d17d4f788566edd90
Turqmelon <devonthome@gmail.com>
2016-06-20 15:19:11 -0400

    teleportPlayerRandomly()

--- src/com/turqmelon/Populace/Utils/PlayerUtils.java
+++ src/com/turqmelon/Populace/Utils/PlayerUtils.java
@@ -4,6 +4,9 @@ import com.turqmelon.Populace.Plot.Plot;
 import com.turqmelon.Populace.Town.Town;
 import org.bukkit.Chunk;
 import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 import java.util.List;
 import java.util.Random;
@@ -13,6 +16,24 @@ import java.util.Random;
  * Project: Populace
  */
 public class PlayerUtils {
+
+    public static void teleportPlayerRandomly(Player player, Town town) {
+        Location target = getRandomTeleportLocationIn(town);
+        if (target != null) {
+            if (player.hasPotionEffect(PotionEffectType.DAMAGE_RESISTANCE)) {
+                player.removePotionEffect(PotionEffectType.DAMAGE_RESISTANCE);
+            }
+            if (player.hasPotionEffect(PotionEffectType.FIRE_RESISTANCE)) {
+                player.removePotionEffect(PotionEffectType.FIRE_RESISTANCE);
+            }
+            player.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, (20 * 20), 50));
+            player.addPotionEffect(new PotionEffect(PotionEffectType.FIRE_RESISTANCE, (20 * 20), 1));
+            player.teleport(target);
+            player.sendMessage(Msg.OK + "Geronimo!");
+        } else {
+            player.sendMessage(Msg.ERR + "Couldn't find a safe destination.");
+        }
+    }
 
     public static Location getRandomTeleportLocationIn(Town town) {
         List<Plot> plots = town.getPlots();
