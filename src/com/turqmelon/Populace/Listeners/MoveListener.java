d55a7807d0842083b7e540887c0e83c382218900
Turqmelon <devonthome@gmail.com>
2016-06-26 15:05:58 -0400

    Re-sync movement (it broke'd)

--- src/com/turqmelon/Populace/Listeners/MoveListener.java
+++ src/com/turqmelon/Populace/Listeners/MoveListener.java
@@ -4,7 +4,6 @@ import com.turqmelon.Populace.Events.Resident.ResidentPlotEnterEvent;
 import com.turqmelon.Populace.Events.Resident.ResidentPlotLeaveEvent;
 import com.turqmelon.Populace.Plot.Plot;
 import com.turqmelon.Populace.Plot.PlotManager;
-import com.turqmelon.Populace.Populace;
 import com.turqmelon.Populace.Resident.Resident;
 import com.turqmelon.Populace.Resident.ResidentManager;
 import com.turqmelon.Populace.Town.PermissionSet;
@@ -14,21 +13,22 @@ import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.event.player.PlayerMoveEvent;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
 /******************************************************************************
- *                                                                            *
+ * *
  * CONFIDENTIAL                                                               *
  * __________________                                                         *
- *                                                                            *
+ * *
  * [2012 - 2016] Devon "Turqmelon" Thome                                      *
- *  All Rights Reserved.                                                      *
- *                                                                            *
+ * All Rights Reserved.                                                      *
+ * *
  * NOTICE:  All information contained herein is, and remains                  *
  * the property of Turqmelon and its suppliers,                               *
  * if any.  The intellectual and technical concepts contained                 *
@@ -38,112 +38,74 @@ import java.util.UUID;
  * Dissemination of this information or reproduction of this material         *
  * is strictly forbidden unless prior written permission is obtained          *
  * from Turqmelon.                                                            *
- *                                                                            *
+ * *
  ******************************************************************************/
 public class MoveListener implements Listener {
 
     private Map<UUID, Location> lastSafePoint = new HashMap<>();
-    private Map<UUID, Location> asyncLocationManagement = new HashMap<>();
 
-    public MoveListener() {
-        new BukkitRunnable() {
-            @Override
-            public void run() {
-                for (Player player : Bukkit.getOnlinePlayers()) {
-                    Resident resident = ResidentManager.getResident(player);
-                    if (resident == null) continue;
-                    Location current = player.getLocation();
+    @EventHandler
+    public void onMove(PlayerMoveEvent event) {
+        Player player = event.getPlayer();
+        Resident resident = ResidentManager.getResident(player);
+        if (resident == null) return;
 
-                    boolean different = false;
-                    Location last = asyncLocationManagement.getOrDefault(player.getUniqueId(), null);
+        Plot plot = PlotManager.getPlot(player.getLocation().getChunk());
 
-                    if (last == null) {
-                        different = true;
-                    } else if (!last.getWorld().getName().equals(current.getWorld().getName())) {
-                        different = true;
-                    } else if (last.getBlockX() != current.getBlockX() || last.getBlockY() != current.getBlockY() || last.getBlockZ() != current.getBlockZ()) {
-                        different = true;
-                    }
+        if (plot != null) {
 
-                    if (different) {
-
-                        Plot plot = PlotManager.getPlot(player.getLocation().getChunk());
-
-                        if (plot != null) {
-
-                            Plot lastPlot = resident.getDisplayEntity().getPlot();
-                            new BukkitRunnable() {
-                                @Override
-                                public void run() {
-                                    boolean denyEntry = false;
-                                    if (lastPlot == null || !lastPlot.getUuid().equals(plot.getUuid())) {
-                                        ResidentPlotEnterEvent enterEvent = new ResidentPlotEnterEvent(plot, resident);
-                                        Bukkit.getPluginManager().callEvent(enterEvent);
-                                        if (enterEvent.isCancelled()) {
-                                            denyEntry = true;
-                                        }
-                                        if (lastPlot != null) {
-                                            Bukkit.getPluginManager().callEvent(new ResidentPlotLeaveEvent(lastPlot, resident));
-                                        }
-                                    }
-                                    if (denyEntry || !plot.can(resident, PermissionSet.ENTRY)) {
-                                        if (lastSafePoint.containsKey(player.getUniqueId())) {
-                                            player.teleport(lastSafePoint.get(player.getUniqueId()));
-                                        } else {
-                                            player.teleport(player.getLocation().clone().add(16, 0, 0));
-                                        }
-                                        player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BASS, 1, 0);
-                                        resident.setFallImmunity(System.currentTimeMillis());
-                                        plot.getPlotChunk().visualize(player, true);
-                                        HUDUtil.sendActionBar(player, "§c§lYou can't enter that area.");
-                                    }
-                                }
-                            }.runTask(Populace.getInstance());
-
-                        } else {
-                            Plot lastPlot = resident.getDisplayEntity().getPlot();
-                            if (lastPlot != null) {
-                                new BukkitRunnable() {
-                                    @Override
-                                    public void run() {
-                                        Bukkit.getPluginManager().callEvent(new ResidentPlotLeaveEvent(lastPlot, resident));
-                                    }
-                                }.runTask(Populace.getInstance());
-                            }
-                        }
-
-                        lastSafePoint.put(player.getUniqueId(), player.getLocation());
-
-                        Town town = plot != null ? plot.getTown() : null;
-                        String msg = resident.getUpdateMessage(town, plot);
-                        if (!msg.equals(resident.getDisplayEntity().getLastSentMessage())) {
-                            resident.getDisplayEntity().setLastSentMessage(msg);
-                            if (msg.length() > 0) {
-                                HUDUtil.sendActionBar(player, msg);
-                            }
-                        }
-
-                        if (resident.isJailed()) {
-                            double range = resident.getJailData().getRange();
-                            range = range * range;
-                            if (player.getLocation().distanceSquared(resident.getJailData().getJailLocation()) > range) {
-                                new BukkitRunnable() {
-                                    @Override
-                                    public void run() {
-                                        player.teleport(resident.getJailData().getJailLocation());
-                                        resident.getJailData().sendExplanation(resident);
-                                    }
-                                }.runTask(Populace.getInstance());
-                            }
-                        }
-
-                        asyncLocationManagement.put(player.getUniqueId(), player.getLocation());
-
-                    }
-
+            Plot lastPlot = resident.getDisplayEntity().getPlot();
+            boolean denyEntry = false;
+            if (lastPlot == null || !lastPlot.getUuid().equals(plot.getUuid())) {
+                ResidentPlotEnterEvent enterEvent = new ResidentPlotEnterEvent(plot, resident);
+                Bukkit.getPluginManager().callEvent(enterEvent);
+                if (enterEvent.isCancelled()) {
+                    denyEntry = true;
+                }
+                if (lastPlot != null) {
+                    Bukkit.getPluginManager().callEvent(new ResidentPlotLeaveEvent(lastPlot, resident));
                 }
             }
-        }.runTaskTimerAsynchronously(Populace.getInstance(), 5L, 5L);
+            if (denyEntry || !plot.can(resident, PermissionSet.ENTRY)) {
+                if (lastSafePoint.containsKey(player.getUniqueId())) {
+                    player.teleport(lastSafePoint.get(player.getUniqueId()));
+                } else {
+                    player.teleport(player.getLocation().clone().add(16, 0, 0));
+                }
+                player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BASS, 1, 0);
+                resident.setFallImmunity(System.currentTimeMillis());
+                plot.getPlotChunk().visualize(player, true);
+                HUDUtil.sendActionBar(player, "§c§lYou can't enter that area.");
+                return;
+            }
+
+        } else {
+            Plot lastPlot = resident.getDisplayEntity().getPlot();
+            if (lastPlot != null) {
+                Bukkit.getPluginManager().callEvent(new ResidentPlotLeaveEvent(lastPlot, resident));
+            }
+        }
+
+        lastSafePoint.put(player.getUniqueId(), player.getLocation());
+
+        Town town = plot != null ? plot.getTown() : null;
+        String msg = resident.getUpdateMessage(town, plot);
+        if (!msg.equals(resident.getDisplayEntity().getLastSentMessage())) {
+            resident.getDisplayEntity().setLastSentMessage(msg);
+            if (msg.length() > 0) {
+                HUDUtil.sendActionBar(player, msg);
+            }
+        }
+
+        if (resident.isJailed()) {
+            double range = resident.getJailData().getRange();
+            range = range * range;
+            if (player.getLocation().distanceSquared(resident.getJailData().getJailLocation()) > range) {
+                player.teleport(resident.getJailData().getJailLocation());
+                resident.getJailData().sendExplanation(resident);
+            }
+        }
+
     }
 
 }
